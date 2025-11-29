dfc01282e8642fd6b088b4e8d638c09f51930d9d
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-28 20:35:55 -0500

    chore:correct routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     ssr: false,
     compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
+    devtools: {enabled: false},
     runtimeConfig: {
         public: {
             apiBase: 'https://simu-credito-platform-6n2e.onrender.com/api'
